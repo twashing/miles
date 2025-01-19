510dae61579a1c04b03bbe95d5e11a403343f0f7
Timoty Washington <twashing@gmail.com>
2025-01-19 18:04:12 -0500

    Troubleshooting PragmataPro download name

--- miles/packages/fonts.scm
+++ miles/packages/fonts.scm
@@ -26,7 +26,10 @@
      (sha256
       (base32
        "0cna4wavnhnb8j8vg119ap8mqkckx04z2gms2hsz4daywc51ghr8"
-       (file-name "PragmataPro-ep6wq.zip?rlkey=gx7rgfbs5frvrnn5e9k1gggid")))))
+       (file-name
+        ;; "PragmataPro-ep6wq.zip?rlkey=gx7rgfbs5frvrnn5e9k1gggid"
+        "PragmataPro-ep6wq.zip?rlkey=gx7rgfbs5frvrnn5e9k1gggid&st=hje0azz0&dl=1"
+        )))))
 
    (build-system font-build-system)
    (arguments
